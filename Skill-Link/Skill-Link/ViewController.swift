e680eba4289b5fa8ffc1b55066910bedf78ac7e9
sayed sadiq <sayedsadiq@sayeds-MacBook-Air.local>
2025-12-01 14:41:20 +0300

    project setup

---
+++ Skill-Link/Skill-Link/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skill-Link
+//
+//  Created by sayed sadiq on 01/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
