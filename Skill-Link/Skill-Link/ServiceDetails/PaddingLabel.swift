2105ed1df8b0775e6a68c4f85af7fb995423bee3
202300428 <202300428@student.polytechnic.bh>
2025-12-08 15:26:45 +0300

    initial work

--- Skill-Link/Skill-Link/ServiceDetails/PaddingLabel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PaddingLabel.swift
-//  Skill-Link
-//
-//  Created by BP-36-201-21 on 08/12/2025.
-//
-
-import Foundation
---
+++ Skill-Link/Skill-Link/ServiceDetails/RoundCardLabel.swift
@@ -0,0 +1,20 @@
+//
+//  RoundCardLabel.swift
+//  Skill-Link
+//
+//  Created by BP-36-201-21 on 08/12/2025.
+//
+
+import UIKit
+
+class RoundCardLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
