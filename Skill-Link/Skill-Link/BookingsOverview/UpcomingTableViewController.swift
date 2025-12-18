42eba067d3889bce3e1e448c412421670cfa92f4
202304651 <245852392+202304651@users.noreply.github.com>
2025-12-18 12:08:06 +0300

    more code

--- Skill-Link/Skill-Link/BookingsOverview/UpcomingTableViewController.swift
+++ Skill-Link/Skill-Link/BookingsOverview/UpcomingTableViewController.swift
@@ -43,13 +43,27 @@ class UpcomingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UpcomingTableViewCell
         let booking = data[indexPath.row]
-        cell.bookingCategory.text = booking.state.rawValue
+        
         cell.serviceTitle.text = booking.title
         cell.providedBy.text = booking.providedBy
         cell.date.text = DateFormatter().string(from: booking.date)
         cell.time.text = booking.time
         cell.location.text = booking.location
         cell.price.text = "\(booking.totalPrice)BD"
+        switch booking.state {
+            case .upcoming:
+            cell.bookingCategory.text = "Upcoming"
+            cell.bookingCategory.backgroundColor = UIColor.tintColor
+            break
+            case .completed:
+            cell.bookingCategory.text = "Completed"
+            cell.bookingCategory.backgroundColor = UIColor.green
+            break
+            case .canceled:
+            cell.bookingCategory.text = "Canceled"
+            cell.bookingCategory.backgroundColor = UIColor.orange
+            break
+        }
         
         return cell
     }
