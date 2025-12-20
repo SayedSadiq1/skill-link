46668e2767d73264547a42961a4c5c89bff0dd94
Sayed <s.h@192-168-100-133.rev.bb.zain.com>
2025-12-20 19:56:50 +0300

    transaction and reviewForm ready for backend (kinda)

---
+++ Skill-Link/Skill-Link/Models/Transaction.swift
@@ -0,0 +1,14 @@
+//
+//  Transaction.swift
+//  Skill-Link
+//
+//  Created by Sayed on 20/12/2025.
+//
+
+struct Transaction {
+    var id: String?
+    var amount: Double
+    var serviceId: String
+    var method: String
+    var createdAt: String
+}
---
+++ Skill-Link/Skill-Link/views/TransactionCell.swift
@@ -0,0 +1,33 @@
+//
+//  TransactionCell.swift
+//  Skill-Link
+//
+//  Created by Sayed on 20/12/2025.
+//
+
+import UIKit
+
+class TransactionCell : UITableViewCell {
+    
+    @IBOutlet weak var serviceLabel: UILabel!
+    @IBOutlet weak var paymentMethodLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var containerView: UIStackView!
+    
+    override func awakeFromNib() {
+           super.awakeFromNib()
+        
+        containerView.backgroundColor = UIColor(hex: "#182E61")
+        containerView.layer.cornerRadius = 20
+        containerView.layer.masksToBounds = true
+        
+       }
+
+       func configure(with transaction: Transaction) {
+           serviceLabel.text = "Payment - "+transaction.serviceId
+           paymentMethodLabel.text = "Paid via: "+transaction.method
+           dateLabel.text = transaction.createdAt
+           amountLabel.text = String(format: "%.2f BD", transaction.amount)
+       }
+}
