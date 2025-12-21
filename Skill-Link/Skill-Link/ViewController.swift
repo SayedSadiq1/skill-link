64a3c1d3676c15ca39ce6c03fa9c126499b40736
202300428 <202300428@student.polytechnic.bh>
2025-12-21 12:27:39 +0300

    make back navigation image work

--- Skill-Link/Skill-Link/ViewController.swift
+++ Skill-Link/Skill-Link/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // 1. Enable user interaction on the ImageView
+    @IBOutlet weak var returnImageView: UIImageView! {
+        didSet {
+            returnImageView.isUserInteractionEnabled = true
+            
+            // 2. Add tap gesture recognizer
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleReturnTap))
+            returnImageView.addGestureRecognizer(tapGesture)
+        }
+    }
+
+    // 3. Handle the tap action
+    @objc private func handleReturnTap() {
+        // Perform your return action
+        //navigationController?.popViewController(animated: true)
+        
+        // Or if presented modally:
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
