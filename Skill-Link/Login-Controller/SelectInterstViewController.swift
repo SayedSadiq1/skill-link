6c3ba9828cd72bb948a4feec52136d619f13b3fa
kinderflip <axpr.bocw@gmail.com>
2025-12-21 11:41:30 +0300

    fixed some issues

---
+++ Skill-Link/Login-Controller/SelectInterstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectInterstViewController.swift
+//  Skill-Link
+//
+//  Created by BP-36-201-11 on 21/12/2025.
+//
+
+import UIKit
+
+class SelectInterstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
