e3045cb9acd4198d928dbfb566230d23ec926408
202300428 <202300428@student.polytechnic.bh>
2025-12-22 12:11:59 +0300

    initial provider view work

--- Skill-Link/Skill-Link/ServiceDetails/ServiceDetailsView.swift
+++ Skill-Link/Skill-Link/ServiceDetails/ServiceDetailsView.swift
@@ -10,12 +10,43 @@ import UIKit
 class ServiceDetailsView: UIViewController {
     @IBOutlet weak var headerStackVIew: UIStackView!
     override func viewDidLoad() {
-        setupUI()
         super.viewDidLoad()
+        setupUI()
     }
     
+    let isProvider: Bool = true
+    
+    @IBOutlet weak var reportStackView: UIStackView!
+    
     func setupUI() {
-        headerStackVIew.layer.borderWidth = 1
-        headerStackVIew.layer.borderColor = UIColor.black.cgColor
+        if (!isProvider) {
+            return
+        }
+        
+        actionBtn.setTitle("Edit service", for: .normal)
+        actionBtn.setImage(UIImage(systemName: "pencil"), for: .normal)
+        cancelBtn.setTitle("Deactivate", for: .normal)
+        cancelBtn.setImage(UIImage(systemName: "xmark.app"), for: .normal)
+    }
+    
+    @IBAction func reportClicked(_ sender: UIButton) {
+        reportStackView.isHidden = false
+    }
+    
+    @IBOutlet weak var actionBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    @IBAction func actionClicked(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "ServiceDetailsStoryboard", bundle: nil)
+        
+        if !isProvider {
+            let controller = storyboard.instantiateViewController(withIdentifier: "serviceBookingViewController")
+            controller.modalPresentationStyle = .fullScreen
+            self.present(controller, animated: true)
+            return
+        }
+        
+        let controller2 = storyboard.instantiateViewController(withIdentifier: "editServiceView")
+        controller2.modalPresentationStyle = .fullScreen
+        self.present(controller2, animated: true)
     }
 }
