10ef102e4797baf4f18fa58bfb11ddb926e6d47b
202300428 <202300428@student.polytechnic.bh>
2025-12-23 11:41:18 +0300

    finish edit design

--- Skill-Link/Skill-Link/ServiceDetails/ServiceDetailsView.swift
+++ Skill-Link/Skill-Link/ServiceDetails/ServiceDetailsView.swift
@@ -8,33 +8,65 @@
 import UIKit
 
 class ServiceDetailsView: UIViewController {
-    @IBOutlet weak var headerStackVIew: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
     
+    let categories = ["Home Maintenance", "Handwork", "Electricity"]
     let isProvider: Bool = true
     
     @IBOutlet weak var reportStackView: UIStackView!
+    @IBOutlet weak var pricingPopupBtn: UIButton!
+    @IBOutlet weak var actionBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    @IBOutlet weak var categoryPopupBtn: UIButton!
     
     func setupUI() {
         if (!isProvider) {
             return
         }
         
-        actionBtn.setTitle("Edit service", for: .normal)
-        actionBtn.setImage(UIImage(systemName: "pencil"), for: .normal)
-        cancelBtn.setTitle("Deactivate", for: .normal)
-        cancelBtn.setImage(UIImage(systemName: "xmark.app"), for: .normal)
+        if pricingPopupBtn != nil {
+            let pricingPopupClosure = {(action : UIAction) in
+                print(action.title)
+            }
+            pricingPopupBtn.menu = UIMenu(children: [
+                UIAction(title: "Hourly", state: .on, handler: pricingPopupClosure),
+                UIAction(title: "Fixed", handler: pricingPopupClosure)
+            ])
+            pricingPopupBtn.showsMenuAsPrimaryAction = true
+            pricingPopupBtn.changesSelectionAsPrimaryAction = true
+        }
+        
+        if categoryPopupBtn != nil {
+            let categoryPopupClosure = {(action : UIAction) in
+            print(action)}
+            var actions: [UIAction] = []
+            for action in categories {
+                actions.append(UIAction(title: action, handler: categoryPopupClosure))
+            }
+            categoryPopupBtn.menu = UIMenu(children: actions)
+            categoryPopupBtn.showsMenuAsPrimaryAction = true
+            categoryPopupBtn.changesSelectionAsPrimaryAction = true
+        }
+        
+        if actionBtn != nil {
+            actionBtn.setTitle("Edit service", for: .normal)
+            actionBtn.setImage(UIImage(systemName: "pencil"), for: .normal)
+        }
+        if cancelBtn != nil {
+            cancelBtn.setTitle("Deactivate", for: .normal)
+            cancelBtn.setImage(UIImage(systemName: "xmark.app"), for: .normal)
+        }
+        
     }
     
     @IBAction func reportClicked(_ sender: UIButton) {
         reportStackView.isHidden = false
     }
     
-    @IBOutlet weak var actionBtn: UIButton!
-    @IBOutlet weak var cancelBtn: UIButton!
+    
     @IBAction func actionClicked(_ sender: Any) {
         let storyboard = UIStoryboard(name: "ServiceDetailsStoryboard", bundle: nil)
         
@@ -48,5 +80,25 @@ class ServiceDetailsView: UIViewController {
         let controller2 = storyboard.instantiateViewController(withIdentifier: "editServiceView")
         controller2.modalPresentationStyle = .fullScreen
         self.present(controller2, animated: true)
+    }
+    
+    // 1. Enable user interaction on the ImageView
+    @IBOutlet weak var returnImageView: UIImageView! {
+        didSet {
+            returnImageView.isUserInteractionEnabled = true
+            
+            // 2. Add tap gesture recognizer
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleReturnTap))
+            returnImageView.addGestureRecognizer(tapGesture)
+        }
+    }
+
+    // 3. Handle the tap action
+    @objc private func handleReturnTap() {
+        // Perform your return action
+        //navigationController?.popViewController(animated: true)
+        
+        // Or if presented modally:
+        dismiss(animated: true, completion: nil)
     }
 }
