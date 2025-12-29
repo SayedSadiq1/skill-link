cfba8b15cc63186ef7aeb746621d813db3763a4d
njnawarr <202203117@student.polytechnic.bh>
2025-12-29 09:45:22 +0300

    stuff done

--- Skill-Link/Skill-Link/HomePage/Service.swift
+++ Skill-Link/Skill-Link/HomePage/Service.swift
@@ -8,11 +8,32 @@
 import Foundation
 
 
-struct Service {
-    let id: String
-    let name: String
+class Service {
+    let id: UUID = UUID.init()
+    let title: String
+    let description: String
     let category: String
     let priceBD: Double
+    let priceType: PriceType
     let rating: Double
-    let providerName: String
+    let provider: UserProfile
+    let available: Bool
+    let disclaimers: [String]
+    
+    init(title: String, description: String, category: String, priceBD: Double, priceType: PriceType, rating: Double, provider: UserProfile, available: Bool, disclaimers: [String]) {
+        self.title = title
+        self.description = description
+        self.category = category
+        self.priceBD = priceBD
+        self.priceType = priceType
+        self.rating = rating
+        self.provider = provider
+        self.available = available
+        self.disclaimers = disclaimers
+    }
+}
+
+enum PriceType {
+    case fixed
+    case hourly
 }
