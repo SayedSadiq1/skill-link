f9f8ffc1a4a6cd1fe6fb5f25433ad418a1df9529
fdjshifh <12345@polytechnic.bh>
2026-01-01 14:21:24 +0300

    aaaaaagasgfsadsubnsakdhjk

--- Skill-Link/Skill-Link/HomePage/ServiceCellTableViewCell.swift
+++ Skill-Link/Skill-Link/HomePage/ServiceCellTableViewCell.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class ServiceCellTableViewCell: UITableViewCell {
-        @IBOutlet weak var serviceNameLabel: UILabel!
-        @IBOutlet weak var priceLabel: UILabel!
-        @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var serviceNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var availabilityLabel: UILabel!
     @IBOutlet weak var checkmarkImage: UIImageView!
+    var serviceData: Service? = nil
+    var parent: UIViewController? = nil
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
-
+    
+    @IBAction func viewDetailsClick(_ sender: Any) {
+        print("jhtyuskjhdgfytsduijhgfdtsrweyuirgjhfvdgy7eurhfj")
+        if serviceData != nil {
+            let serviceDetailsStoryboard = UIStoryboard(name: "ServiceDetailsStoryboard", bundle: nil)
+            if let serviceDetails = serviceDetailsStoryboard.instantiateViewController(withIdentifier: "serviceDetailsPage") as? ServiceDetailsViewController {
+                serviceDetails.service = serviceData
+                parent!.navigationController?.pushViewController(serviceDetails, animated: true)
+            }
+        }
+    }
 }
